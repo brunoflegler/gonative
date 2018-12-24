7eda6ee2effc324c86a5a285112353f42d729f44
Bruno Flegler Dal Col <bruno.flegler@gmail.com>
2018-12-24 13:35:54 -0200

    first commit

---
+++ aulas/modulo3/src/index.js
@@ -0,0 +1,15 @@
+import "~/config/DevToolsConfig";
+import "~/config/ReactotronConfig";
+
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
